a7095c1ed6ed75d68a0766a639e930cb0e35198e
DESKTOP-KPJAGTA\ILTS_VLE <vle@ilts.com>
2019-06-19 17:59:38 +0700

    add adc

--- app/tds.c
+++ app/tds.c
@@ -9,6 +9,42 @@ void delay(unsigned long count) {
         nop();
 }
 
+void init_adc() {
+    ADC_CSR = 0;
+    ADC_CR1 = 0;
+    ADC_CR2 = 0;
+    ADC_CR3 = 0;
+
+/*
+    ADC_DRH = 0;
+    ADC_DRL = 0;
+    ADC_TDRH = 0;
+    ADC_TDRL = 0;
+    ADC_HTRH = 0;
+    ADC_HTRL = 0;
+    ADC_LTRH = 0;
+    ADC_LTRL = 0;
+    ADC_AWSRH = 0;
+    ADC_AWSRL = 0;
+    ADC_AWCRH = 0;
+    ADC_AWCRL = 0;
+*/
+    ADC_CSR = 2; // Select channel 2 (AIN2=PC4)
+
+    ADC_CR1 |= ADC_CR1_ADON; // ADON
+    ADC_CR2 &= ~ADC_CR2_ALIGN; // Align left
+
+    delay(1000); // Give little time to be ready for first conversion
+}
+
+uint16_t analog_read() {
+    ADC_CR1 &= ~ADC_CR1_CONT; // Single conversion mode
+    ADC_CR1 |= ADC_CR1_ADON; // Start conversion
+    do { nop(); } while ((ADC_CSR >> 7) == 0);
+    ADC_CSR &= ~ADC_CSR_EOC; // Clear "End of conversion"-flag
+    return (ADC_DRH << 2) | (ADC_DRL >> 6);  // Left aligned
+}
+
 int uart_write(const char *str) {
     char i;
     for(i = 0; i < strlen(str); i++) {
@@ -18,7 +54,7 @@ int uart_write(const char *str) {
     return(i); // Bytes sent
 }
 
-void uart_init(void)
+void init_uart(void)
 {
     /* Set clock to full speed (16 Mhz) */
     CLK_CKDIVR = 0;
@@ -30,11 +66,13 @@ void uart_init(void)
     // 9600 baud: UART_DIV = 16000000/9600 ~ 1667 = 0x0683
     UART1_BRR2 = 0x03; UART1_BRR1 = 0x68; // 0x0683 coded funky way (see ref manual)
 }
+
 int main(void)
 {
-    uart_init();
+    init_uart();
+    init_adc();
     while(1) {
-        uart_write("Hello World!\r\n");
+        uart_write("Hello World uart adc \r\n");
         delay(400000L);
     }
 }
